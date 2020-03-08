511cb78a4d6fb333b27238a8835b0846fd0456cc
Gabriel Oliveira Aragão <31046857+linknasubi@users.noreply.github.com>
2020-03-08 05:04:40 -0300

    Update Neural_Net.py

--- Neural_Net.py
+++ Neural_Net.py
@@ -26,10 +26,10 @@ labels = labels.reshape(100,1)
 weights_layers_hidden = []
 
 bias = np.random.rand(1)
+lrn_rate = 0.2
 
 
-
-hidden_layers = 2 # Refere-se a quantidade de camadas desejáveis
+hidden_layers = 3 # Refere-se a quantidade de camadas desejáveis
 layer_counter = 0
 nodes = 4
 nodes_arrays = np.array([])
@@ -43,17 +43,15 @@ output_layer = np.array([])
 derv_layers_arrays = [] #Armazena as derivadas referentes a cada peso
 
 '''
-
 A sequência abaixo gera os valores corrrespondentes aos pesos que serão
 usados entre o input e a primeira camada hidden
-
 '''
 
 
 weight = np.random.rand(len(feature_set[0]), nodes) #Aqui é gerado os weights entre o input e a primeira camada hidden
 weights_layers_input.append(weight)
 
-nodes_arrays = np.dot(feature_set,weights_layers_input)+bias
+nodes_arrays = np.dot(feature_set,weight)+bias
 
 
 nodes_arrays = sigmoid(nodes_arrays)
@@ -67,12 +65,11 @@ nodes_layers_arrays.append(nodes_arrays)
 '''
 A sequência abaixo gera os valores correspondentes ao cálculo entre os pesos
 das hidden layers em questão.
-
 '''
 
 
 
-for i in range(hidden_layers):
+for i in range(1, hidden_layers):
     weight = np.random.rand(nodes, nodes)
     weights_layers_hidden.append(weight)
 
@@ -93,23 +90,21 @@ output_layer_dot = np.dot(nodes_layers_arrays[-1], weights_layers_output) + bias
 
 output_layer_sig = sigmoid(output_layer_dot)
 
-error_cost = (1/len(nodes_layers_arrays[-1][0]))*((output_layer_sig - labels)**2)
+
+error_cost = (1/len(nodes_layers_arrays[-1]))* sum(((output_layer_sig - labels)**2))
 print(error_cost)
 
 
 for i in range(len(nodes_layers_arrays)):
     layer_counter-=1
     
-    dcost_da = (2/len(nodes_layers_arrays[layer_counter][0])) * (output_layer_sig - labels)
+    dcost_da = (2/len(nodes_layers_arrays[layer_counter])) * sum((output_layer_sig - labels))
     da_dz = sigmoid_derv(output_layer_dot)
-    dz_dw = nodes_layers_arrays[layer_counter]
+    dz_dw = nodes_layers_arrays[layer_counter].T
     
-    dcost_w = np.dot(dz_dw.T, dcost_da*da_dz)
+    dcost_w = np.dot(dz_dw, dcost_da*da_dz)
     
-    derv_layers_arrays.append(dcost_w)
-
-
-
+    weights_layers_hidden[layer_counter] = weights_layers_hidden[layer_counter] - (dcost_w*error_cost*lrn_rate)
 
 
 
